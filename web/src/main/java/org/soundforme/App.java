d6963652a6e748eba7a7453103cbe98c7b682b9c
tuBARetkin <2baret@gmail.com>
2015-04-20 23:47:53 +0400

    index controller

--- web/src/main/java/org/soundforme/App.java
+++ web/src/main/java/org/soundforme/App.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.inject.Inject;
 
